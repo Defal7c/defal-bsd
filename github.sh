a7ab8e786723773cc0a607b5f2b4e8ac9fb7f38c
defal7c <mth.fe.a@protonmail.com>
2021-11-22 14:43:37 -0300

    test from openbsd

---
+++ github.sh
@@ -0,0 +1,7 @@
+# Stuff to remember
+
+EMAIL=
+NAME=
+
+git config --global user.name defal7c
+git config --global user.email $EMAIL
--- st-0.8.4/config.mk
+++ st-0.8.4/config.mk
@@ -10,7 +10,7 @@ MANPREFIX = $(PREFIX)/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-PKG_CONFIG = pkg-config
+PKG_CONFIG = pkgconf
 
 # includes and libs
 INCS = -I$(X11INC) \
